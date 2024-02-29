10b4e88b1d90b8f69663330eeb6d5c52daddfd9f
Mochamad Wildani Azizi <wwilldanazizi@gmail.com>
2024-03-01 00:23:15 +0700

    Add View Part

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,4 +9,13 @@ class WelcomeController extends Controller
     public function hello() {
         return 'Hello World';
     }
+
+    public function greeting(){
+        return view('blog.hello')
+            ->with('name', 'Andi')
+            ->with('occupation', 'Astronaut');
+        //when type like above, the parameter 'name' have a value that
+        //is Andi, so if you call in hello.blade.php the result if you
+        //call $name, the result will be Andi
+    } 
 }
--- routes/web.php
+++ routes/web.php
@@ -93,4 +93,5 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
-   
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
